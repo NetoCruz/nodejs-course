e1ae1df314b2ef661225ce6b263318b06988176f
unknown <galaxy.net0710@gmail.com>
2019-08-29 16:29:43 -0500

    Server Express.js

---
+++ movies-api/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+
+const { config } = require('./config/index');
+const moviesApi = require('./routes/movies.js')
+moviesApi(app)
+// app.get('/', function(req, res) {
+//   res.send('hello world');
+// });
+
+// app.get('/json', function(req, res) {
+//   res.json({ hello: 'world' });
+// });
+
+
+app.listen(config.port, function() {
+  console.log(`Listening http://localhost:${config.port}`);
+});
